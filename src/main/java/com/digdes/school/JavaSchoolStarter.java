6632e666c78c170a81911415402f4b367d2f69fa
Igor Karpiy <igorkarpy@gmail.com>
2023-03-26 03:17:10 +0300

    Initial commit

---
+++ src/main/java/com/digdes/school/JavaSchoolStarter.java
@@ -0,0 +1,19 @@
+package com.digdes.school;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class JavaSchoolStarter {
+    //Дефолтный конструктор
+    public JavaSchoolStarter(){
+
+    }
+
+    //На вход запрос, на выход результат выполнения запроса
+    public List<Map<String,Object>> execute(String request) throws Exception {
+        //Здесь начало исполнения вашего кода
+
+        return new ArrayList<>();
+    }
+}
